4b3159d26d34a07177b549dd69b73a8f193b1dc0
Reinaldo Borges Salla <reinaldo.salla@pampa.compasso>
2020-04-15 17:21:10 -0300

    Created Title and Items component

--- src/components/Women/women.js
+++ src/components/Women/women.js
@@ -4,22 +4,40 @@ import { Link } from "react-router-dom";
 import { data } from "../data.js";
 import "./women.css";
 
-export default function Women() {
+function Title() {
+  return (
+    <div className="title-women">
+      <p>All items - for women</p>
+    </div>
+  );
+}
+
+function Items() {
   const productItems = data.map(product => 
     <div key={product.id} className="item-women">
-      <Link to={`/products/${product.id}`}>
+      <Link to={`/product/${product.id}`}>
         <img alt={product.name} src={product.img} />
+        <h1 id="product-name-women">{product.name}</h1>
+        <h4 id="product-description-women">{product.description}</h4>
       </Link>
+      <div className="price-add-women">
+        <h5 id="product-price">${product.price}</h5>
+        <Icon small id="add-icon">add_shopping_cart</Icon>
+      </div>
     </div>
   );
   return (
+    <div className="items-women">
+      {productItems}
+    </div>
+  );
+}
+
+export default function Women() {
+  return (
     <div>
-      <div className="title-women">
-        <p>All items - women's</p>
-      </div>
-      <div className="items-women">
-        {productItems}
-      </div>
+      <Title />
+      <Items />
     </div>
   );
 }
